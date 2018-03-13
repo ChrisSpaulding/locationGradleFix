5052eef1b22655ad96157b40978b38bac567efb7
ChrisSpaulding1991@gmail.com <ChrisSpaulding1991@gmail.com>
2018-03-13 12:54:28 -0600

    Initial commit

---
+++ app/src/main/java/com/example/dell/locationgradlefix/GeoLocater.kt
@@ -0,0 +1,12 @@
+package com.example.dell.locationgradlefix
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class GeoLocater : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_geo_locater)
+    }
+}
